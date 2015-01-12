12615a13cc22e2270537db54d53b9e56416f0257
Harry Steinhilber, Jr. <harry.steinhilber@gmail.com>
2015-01-12 13:46:46 -0500

    test to ensure casing is correct for verification signature

--- SQRL.Test/SqrlUriTests.cs
+++ SQRL.Test/SqrlUriTests.cs
@@ -11,7 +11,7 @@ namespace SQRL.Test {
         [TestCase("sqrl://example.com/hsteinhilber|sqrl/login.php?NONCE", "example.com/hsteinhilber")]
         [TestCase("sqrl://example.com:8080/hsteinhilber|sqrl/login.php?NONCE", "example.com/hsteinhilber")]
         public void SiteKeyString_should_be_correct(string uriString, string siteKeyString) {
-            SqrlUri uri = new SqrlUri(uriString);
+            var uri = new SqrlUri(uriString);
             Assert.That(uri.SiteKeyString, Is.EqualTo(siteKeyString));
         }
 
@@ -20,7 +20,7 @@ namespace SQRL.Test {
         [TestCase("sqrl://example.com:8080", 8080)]
         [TestCase("qrl://example.com:1024", 1024)]
         public void Port_should_be_correct(string uriString, int expectedPort) {
-            SqrlUri uri = new SqrlUri(uriString);
+            var uri = new SqrlUri(uriString);
             Assert.That(uri.Port, Is.EqualTo(expectedPort));
         }
 
@@ -32,10 +32,24 @@ namespace SQRL.Test {
             Assert.That(() => new SqrlUri(uriString), Throws.InstanceOf<NotSupportedException>());
         }
 
-        [TestCase("sqrl://example.com")]
-        [TestCase("qrl://example.com")]
+        [TestCase("sqrl://example.com/")]
+        [TestCase("qrl://example.com/")]
         public void Supported_schemes_should_include_sqrl_and_qrl(string uriString) {
             Assert.That(() => new SqrlUri(uriString), Throws.Nothing);
+        }
+
+        [TestCase("SQRL://example.com/", "sqrl://example.com/")]
+        [TestCase("sqrl://Example.Com/", "sqrl://example.com/")]
+        public void Scheme_and_host_should_always_be_lowercase(string uriString, string expected) {
+            var uri = new SqrlUri(uriString);
+            Assert.That(uri.ToString(), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Path_should_retain_casing() {
+            var expected = "sqrl://example.com/PathWithCasing?Id=SomeNonce";
+            var uri = new SqrlUri(expected);
+            Assert.That(uri.ToString(), Is.EqualTo(expected));
         }
     }
 }
