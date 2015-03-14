1ca66de8c44a0a1049dfc0cfa9537a2cdc7dd687
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2015-03-13 20:41:12 -0400

    Added first draft of Enhash method

--- Src/SQRL.Shared/Security/SecurityExtensions.cs
+++ Src/SQRL.Shared/Security/SecurityExtensions.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.Text;
+using Windows.Security.Cryptography.Core;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace SQRL.Security {
     public static class SecurityExtensions {
+        public static byte[] Enhash(this byte[] source) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (source.Length != 32) throw new ArgumentException("source", "source must be a 256 bit array");
+
+            var hasher = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256);
+            byte[] input;
+            // TODO: This is generating too many arrays for garbage collection.
+            //       In addition, none of these allocated buffers are being cleared properly.
+            var result = input = hasher.HashData(source.AsBuffer()).ToArray();
+            for (int i = 1; i < 16; ++i) {
+                input = hasher.HashData(input.AsBuffer()).ToArray();
+                result.Xor(input);
+            }
+
+            return result;
+        }
+
         public static byte[] Xor(this byte[] left, byte[] right) {
             if (left == null) throw new ArgumentNullException("left");
             if (right == null) throw new ArgumentNullException("right");
---
+++ Tests/SQRL.Shared.Test/Security/EnhashTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQRL.Security.Test
+{
+    [TestClass]
+    public class EnhashTests
+    {
+        [TestMethod]
+        public void It_should_throw_if_source_array_is_null() {
+            Assert.ThrowsException<ArgumentNullException>(() => SecurityExtensions.Enhash(null));
+        }
+
+        [TestMethod]
+        public void It_should_throw_if_source_array_is_not_256_bit() {
+            Assert.ThrowsException<ArgumentException>(() => SecurityExtensions.Enhash(new byte[31]));
+        }
+
+        [TestMethod]
+        public void It_should_return_a_256_bit_array() {
+            var input = new byte[32];
+            var result = SecurityExtensions.Enhash(input);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length == 32, "returned value is {0} bits", result.Length * 8);
+        }
+
+        [TestMethod]
+        public void It_should_return_a_different_array_than_input() {
+            var input = new byte[32];
+            var result = SecurityExtensions.Enhash(input);
+            Assert.AreNotSame(input, result);
+            CollectionAssert.AreNotEqual(input, result);
+        }
+    }
+}
